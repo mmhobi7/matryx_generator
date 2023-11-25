800528a5f0617bd14b947d265262a0d585edc704
M <woohoomoo2u@gmail.com>
2023-11-25 18:13:27 -0500

    filter quarter

--- src/main.rs
+++ src/main.rs
@@ -294,7 +294,7 @@ fn filter_red(canvas: &mut Canvas) {
     }
 }
 
-fn filter_red_dark(canvas: &mut Canvas) {
+fn filter_quarter(canvas: &mut Canvas) {
     for y in 0..canvas.height {
         for x in 0..canvas.width {
             let mut curr_pixel: f32 = canvas.get_pixel(x, y)[0];
@@ -367,7 +367,7 @@ fn main() {
         if hists.load(Ordering::Acquire) <= 24 {
             // filter_darken(&mut canvas_clock, 0.003922);
             // filter_red(&mut canvas_clock);
-            filter_red_dark(&mut canvas_clock);
+            filter_quarter(&mut canvas_clock);
             client.send_brightness(1);
             client.send_frame(canvas_clock.pixels());
         } else {
@@ -376,14 +376,14 @@ fn main() {
             // let mut canvas4 = canvas_wave.clone();
             // filter_background(&mut canvas3, &mut canvas2);
             // filter_bright_foreground(&mut canvas4, &mut canvas_wave, 0.01);
-            filter_bright_background(&mut canvas_wave, &mut canvas_clock, 0.1);
+filter_bright_background(&mut canvas_wave, &mut canvas_clock, 0.1);
             if shifter == (SHIFTER_START * (-1.0)) {
                 shifter = SHIFTER_START;
             } else {
                 shifter = shifter + 1.0;
             }
             filter_hue_shift(&mut canvas_wave, shifter);
-            client.send_brightness(100);
+                        client.send_brightness(100);
             client.send_frame(canvas_wave.pixels());
         }
         frame_timer.wait_for_next_frame();
